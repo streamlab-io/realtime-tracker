bdca97108dd85ee366af1077c60be7932c632749
zizohassan <zizo1999882gmail.com>
2017-03-19 21:55:58 +0200

    update files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('style')
-    <title> StreamLab TestChat Login</title>
+    <title> StreamLab RealTime Tracker Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 @endsection
 
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-lg-12 text-center">
             <h1>
-                StreamLab TestChat
+                StreamLab RealTime Tracker Login
             </h1>
         </div>
         <div class="col-md-8 col-md-offset-2">
